c2ca852d0282bb5c01be56776ca69faf55cba1c2
37237002 <37237002@L829WD220016.edu.cbe.ab.ca>
2024-12-04 14:16:13 -0700

    made the sprite able to rotate

--- sprites/SimpleSprite.java
+++ sprites/SimpleSprite.java
@@ -6,15 +6,22 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 public class SimpleSprite implements DisplayableSprite {
-
+	
+	
+	private static Image[] rotatedImages = new Image[360];
 	private static Image image;	
 	private double centerX = 0;
 	private double centerY = 0;
 	private double width = 50;
 	private double height = 50;
 	private boolean dispose = false;	
-
+	private Image rotatedImage;
+	
+	private double ACCELERATION = 400;    
 	private final double VELOCITY = 200;
+	private double ROTATION_SPEED = 150;	//degrees per second	
+	private double currentAngle = 0;
+	private int currentImageAngle = 0;
 
 	public SimpleSprite(double centerX, double centerY, double height, double width) {
 		this(centerX, centerY);
@@ -29,19 +36,25 @@ public class SimpleSprite implements DisplayableSprite {
 		this.centerX = centerX;
 		this.centerY = centerY;
 		
-		if (image == null) {
-			try {
-				image = ImageIO.read(new File("res/simple-sprite.png"));
+		Image image = null;
+		try {
+			image = ImageIO.read(new File("res/simple-sprite.png"));
+		}
+		catch (IOException e) {
+			System.out.print(e.toString());
+		}
+
+		if (image != null) {
+			for (int i = 0; i < 360; i++) {
+				rotatedImages[i] = ImageRotator.rotate(image, i);			
 			}
-			catch (IOException e) {
-				System.out.println(e.toString());
-			}		
 		}		
 	}
 
 	public Image getImage() {
-		return image;
+		return rotatedImages[(int)currentAngle];
 	}
+	
 	
 	//DISPLAYABLE
 	
@@ -97,20 +110,30 @@ public class SimpleSprite implements DisplayableSprite {
 
 		//LEFT	
 		if (keyboard.keyDown(37)) {
-			velocityX = -VELOCITY;
+			currentAngle -= (ROTATION_SPEED * (actual_delta_time * 0.001));
 		}
 		//UP
 		if (keyboard.keyDown(38)) {
-			velocityY = -VELOCITY;			
-		}
+			double angleInRadians = Math.toRadians(currentAngle);
+			velocityX += Math.cos(angleInRadians) * ACCELERATION * actual_delta_time * 0.001;
+			velocityY += Math.sin(angleInRadians) * ACCELERATION * actual_delta_time * 0.001;
 		// RIGHT
+		}
 		if (keyboard.keyDown(39)) {
-			velocityX += VELOCITY;
+			currentAngle += (ROTATION_SPEED * (actual_delta_time * 0.001));
 		}
 		// DOWN
 		if (keyboard.keyDown(40)) {
 			velocityY += VELOCITY;			
 		}
+		if (currentAngle >= 360) {
+	    	currentAngle -= 360;
+	    }
+	    if (currentAngle < 0) {
+	    	currentAngle += 360;
+	    }	
+	    
+	    currentAngle %= 360;
 
 		double deltaX = actual_delta_time * 0.001 * velocityX;
 		double deltaY = actual_delta_time * 0.001 * velocityY;
