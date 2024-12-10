7422c871c642de10cdb547f5cc73cd86e1032096
37237002 <37237002@L829WD220016.edu.cbe.ab.ca>
2024-12-10 12:35:12 -0700

    finished the three ufos

--- sprites/PurpleUfo.java
+++ sprites/PurpleUfo.java
@@ -6,54 +6,54 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 public class PurpleUfo implements DisplayableSprite {
-	
-	
+
 	private static Image[] rotatedImages = new Image[360];
-	private static Image image;	
+	private static Image image;
 	private double centerX = 0;
 	private double centerY = 0;
 	private double width = 64;
 	private double height = 64;
-	private boolean dispose = false;	
+	private boolean dispose = false;
 	private Image rotatedImage;
-	
-	
+	private double reloadTime = 0;
+	private double velocityX = 0;
+	private double velocityY = 0;
+
 	private int currentFrame = 0;
 	private long elapsedTime = 0;
 	private final static int FRAMES = 5;
 	private double framesPerSecond = 3;
 	private double milliSecondsPerFrame = 1000 / framesPerSecond;
 	private static Image[] frames = new Image[FRAMES];
-	private static boolean framesLoaded = false;	
-	
-	private double ACCELERATION = 10000;    
+	private static boolean framesLoaded = false;
+
+	private double ACCELERATION = 10000;
 	private final double VELOCITY = 40000;
-	private double ROTATION_SPEED = 150;	//degrees per second	
+	private double ROTATION_SPEED = 150; // degrees per second
 	private int currentAngle = 0;
 	private int currentImageAngle = 0;
 
 	public PurpleUfo(double centerX, double centerY, double height, double width) {
 		this(centerX, centerY);
-		
+
 		this.height = height;
 		this.width = width;
 	}
-	
+
 	public PurpleUfo(double centerX, double centerY, double framesPerSecond) {
 
 		this.centerX = centerX;
 		this.centerY = centerY;
-		
-		
 
 		this.framesPerSecond = framesPerSecond;
 		this.milliSecondsPerFrame = 1000 / framesPerSecond;
 		long startTime = System.currentTimeMillis();
-		
+
 		/*
-		 * Loading of the images into an array. The image files are named in sequence by the online tool that split
-		 * the animated GIF, and String formatting is used to re-create the correct file name. Note the use of
-		 * a static array to store the images... this ensures that only a single copy of each rotated image is
+		 * Loading of the images into an array. The image files are named in sequence by
+		 * the online tool that split the animated GIF, and String formatting is used to
+		 * re-create the correct file name. Note the use of a static array to store the
+		 * images... this ensures that only a single copy of each rotated image is
 		 * stored, even if there are multiple instances of this sprite.
 		 */
 		if (framesLoaded == false) {
@@ -61,26 +61,25 @@ public class PurpleUfo implements DisplayableSprite {
 				String filename = "res/ufo_purple/purpleufo_" + String.format("%01d", frame) + ".png";
 				try {
 					frames[frame] = ImageIO.read(new File(filename));
-				}
-				catch (IOException e) {
+				} catch (IOException e) {
 					System.err.println(e.toString());
-					
-				}		
+
+				}
 			}
-			
+
 			if (frames[0] != null) {
 				framesLoaded = true;
 			}
-			
+
 			System.out.println(frames.toString());
 //			System.exit(0);
 
-		}		
+		}
 	}
-	
+
 	public PurpleUfo(double centerX, double centerY) {
 		this(centerX, centerY, 3);
-		
+
 //		Image image = null;
 //		try {
 //			image = ImageIO.read(new File("res/ufo_green/sprite_0.png"));
@@ -99,14 +98,13 @@ public class PurpleUfo implements DisplayableSprite {
 	public Image getImage() {
 		return ImageRotator.rotate(frames[currentFrame], currentAngle);
 	}
-	
-	
-	//DISPLAYABLE
-	
+
+	// DISPLAYABLE
+
 	public boolean getVisible() {
 		return true;
 	}
-	
+
 	public double getMinX() {
 		return centerX - (width / 2);
 	}
@@ -138,61 +136,63 @@ public class PurpleUfo implements DisplayableSprite {
 	public double getCenterY() {
 		return centerY;
 	};
-	
-	
+
 	public boolean getDispose() {
 		return dispose;
 	}
 
 	public void update(Universe universe, long actual_delta_time) {
-		
+
 		double velocityX = 0;
 		double velocityY = 0;
-		
-		KeyboardInput keyboard = KeyboardInput.getKeyboard();
-		
-		
 
-		//LEFT	numpad 1
+		KeyboardInput keyboard = KeyboardInput.getKeyboard();
+
+		// LEFT (1)
 		if (keyboard.keyDown(97)) {
 			currentAngle -= (ROTATION_SPEED * (actual_delta_time * 0.001));
 		}
-		//UP ( numpad 5)
+		// UP (5)
 		if (keyboard.keyDown(101)) {
 			double angleInRadians = Math.toRadians(currentAngle);
 			velocityX += Math.cos(angleInRadians) * ACCELERATION * actual_delta_time * 0.001;
 			velocityY += Math.sin(angleInRadians) * ACCELERATION * actual_delta_time * 0.001;
-		// RIGHT
+
 		}
+		// RIGHT (3)
 		if (keyboard.keyDown(99)) {
 			currentAngle += (ROTATION_SPEED * (actual_delta_time * 0.001));
 		}
-		// DOWN
+		// DOWN (2)
 		if (keyboard.keyDown(98)) {
 			double angleInRadians = Math.toRadians(currentAngle);
 			velocityX -= Math.cos(angleInRadians) * ACCELERATION * actual_delta_time * 0.001;
 			velocityY -= Math.sin(angleInRadians) * ACCELERATION * actual_delta_time * 0.001;
 		}
 		if (currentAngle >= 360) {
-	    	currentAngle -= 360;
-	    }
-	    if (currentAngle < 0) {
-	    	currentAngle += 360;
-	    }	
-	    
-	    currentAngle %= 360;
+			currentAngle -= 360;
+		}
+		if (currentAngle < 0) {
+			currentAngle += 360;
+		}
 
+		currentAngle %= 360;
+		reloadTime -= actual_delta_time;
+
+		// SHOOT (0)
+		if (keyboard.keyDown(96)) {
+			shoot(universe);
+		}
 		double deltaX = actual_delta_time * 0.001 * velocityX;
 		double deltaY = actual_delta_time * 0.001 * velocityY;
-		
-		
+
 		boolean collidingBarrierX = checkCollisionWithBarrier(universe.getSprites(), deltaX, 0);
 		boolean collidingBarrierY = checkCollisionWithBarrier(universe.getSprites(), 0, deltaY);
-    	
-		if(!collidingBarrierY) {
+
+		if (!collidingBarrierY) {
 			this.centerY += deltaY;
 		}
-		if(!collidingBarrierX) {
+		if (!collidingBarrierX) {
 			this.centerX += deltaX;
 		}
 		elapsedTime += actual_delta_time;
@@ -201,28 +201,48 @@ public class PurpleUfo implements DisplayableSprite {
 
 	}
 
-
 	@Override
 	public void setDispose(boolean dispose) {
 		this.dispose = true;
 	}
-	
+
 	private boolean checkCollisionWithBarrier(ArrayList<DisplayableSprite> sprites, double deltaX, double deltaY) {
 
 		boolean colliding = false;
 
 		for (DisplayableSprite sprite : sprites) {
 			if (sprite instanceof BarrierSprite) {
-				if (CollisionDetection.overlaps(this.getMinX() + deltaX + 8, this.getMinY() + deltaY + 8, 
-						this.getMaxX()  + deltaX - 8, this.getMaxY() + deltaY - 6, 
-						sprite.getMinX(),sprite.getMinY(), 
+				if (CollisionDetection.overlaps(this.getMinX() + deltaX + 8, this.getMinY() + deltaY + 8,
+						this.getMaxX() + deltaX - 8, this.getMaxY() + deltaY - 6, sprite.getMinX(), sprite.getMinY(),
 						sprite.getMaxX(), sprite.getMaxY())) {
 					colliding = true;
-					break;					
+					break;
 				}
 			}
 		}
 		return colliding;
+	}
+
+	public void shoot(Universe universe) {
+
+		if (reloadTime <= 0) {
+			double currentVelocity = Math.sqrt((velocityX * velocityX) + (velocityY * velocityY));
+			double bulletVelocity = 750; // + currentVelocity;
+			double ratio = (bulletVelocity / currentVelocity);
+//			 = ratio * velocityX + velocityX;
+//			double bulletVelocityY = ratio * velocityY + velocityY;
+			double angleInRadians = Math.toRadians(currentAngle);
+			double bulletVelocityX = Math.cos(angleInRadians) * bulletVelocity + velocityX;
+			double bulletVelocityY = Math.sin(angleInRadians) * bulletVelocity + velocityY;
+
+			double bulletCurrentX = this.getCenterX();
+			double bulletCurrentY = this.getCenterY();
+
+			bullet_sprite bullet = new bullet_sprite(bulletCurrentX, bulletCurrentY, bulletVelocityX, bulletVelocityY, "res/PurpleLaser.png");	
+			universe.getSprites().add(bullet);
+			reloadTime = 100;
+
+		}
 	}
 
 }
