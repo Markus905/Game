2797e472805a7dc8f3be7d4cd7f3df4ab25b936c
27080005 <27080005@L829WD220010.edu.cbe.ab.ca>
2024-12-10 13:31:53 -0700

    added randomly generating map

--- animations/ShellAnimation.java
+++ animations/ShellAnimation.java
@@ -1,41 +1,49 @@
-
 public class ShellAnimation implements Animation {
-
 	private Universe current = new tankmaplayout();
 	private boolean universeSwitched = false;
 	private boolean animationComplete = false;
+	private int universeCount = 0;
 	
 	public Universe getCurrentUniverse() {
-		return current;
+		if(universeCount == 0) {
+			return new tankmaplayout();
+		} else{
+			return new MainUniverse();
+		}
+		
+		
 	}
+	
 	
 	@Override
 	public boolean getUniverseSwitched() {
 		return universeSwitched;
 	}
-
 	@Override
 	public void acknowledgeUniverseSwitched() {
 		this.universeSwitched = false;		
 	}
-
 	@Override
 	public boolean isComplete() {
 		return animationComplete;
 	}
-
 	@Override
 	public void setComplete(boolean complete) {
 		this.animationComplete = true;		
 	}
-
 	@Override
 	public void update(AnimationFrame frame, long actual_delta_time) {		
 		if ( KeyboardInput.getKeyboard().keyDownOnce(27)) {
-			animationComplete = true;
-		}				
+			if(this.universeCount < 1 && animationComplete == false) {
+				universeCount++;
+				this.universeSwitched = true;
+			} else {
+				animationComplete = true;
+			}
+			
+		}
+		
 	}
-
 	public Universe switchUniverse(Object event) {
 		animationComplete = true;
 		return current;
---
+++ universes/MainUniverse.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+public class MainUniverse implements Universe {
+	private boolean complete = false;
+	private boolean gameOver = false;
+	private Background background;
+	private ArrayList<Background> backgrounds;
+	
+	private DisplayableSprite player;
+	
+	
+	private ArrayList<DisplayableSprite> sprites = new ArrayList<DisplayableSprite>();
+	
+	private double xCenter;
+	private double yCenter;
+	
+	public MainUniverse() {
+		background = new RandomlyGeneratedMap();
+		backgrounds = new ArrayList<Background>();
+		backgrounds.add(new GameBackground());
+		backgrounds.add(background);
+		ArrayList<DisplayableSprite> barriers = ((RandomlyGeneratedMap) background).getBarriers();
+		
+		player = new GreenUfo(300,250);
+		
+		sprites.add(player);
+		sprites.addAll(barriers);
+	
+	}
+	public double getScale() {
+		return 0.81;
+	}
+	public double getXCenter() {
+		return 750;
+	}
+	public double getYCenter() {
+		return 550;
+	}
+	public void setXCenter(double xCenter) {
+		this.xCenter = xCenter;
+	}
+	public void setYCenter(double yCenter) {
+		this.yCenter = yCenter;
+	}
+	public boolean isComplete() {
+		return complete;
+	}
+	public void setComplete(boolean complete) {
+		complete = true;
+	}
+	
+	public boolean getGameOver() {
+		return gameOver;
+	}
+	
+	
+	@Override
+	public ArrayList<Background> getBackgrounds() {
+		return backgrounds;
+	}
+	public DisplayableSprite getPlayer1() {
+		return null;
+	}
+	
+	public ArrayList<DisplayableSprite> getSprites() {
+		return sprites;
+	}
+	public boolean centerOnPlayer() {
+		return true;
+	}
+ 	public String toString() {
+		return "game game yay !";
+	}
+	public void update(Animation animation, long actual_delta_time) {
+		
+		for (int i = 0; i < sprites.size(); i++) {
+			DisplayableSprite sprite = sprites.get(i);
+			sprite.update(this, actual_delta_time);
+		}
+		
+	}
+}
